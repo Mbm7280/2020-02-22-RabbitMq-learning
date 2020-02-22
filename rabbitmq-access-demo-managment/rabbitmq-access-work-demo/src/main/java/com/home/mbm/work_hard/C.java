18d3eb9a56ec79ffa66ac56dd066f966f236080c
Mbm7280 <56158949+Mbm7280@users.noreply.github.com>
2020-02-22 16:48:28 +0800

    Create C.java

---
+++ rabbitmq-access-demo-managment/rabbitmq-access-work-demo/src/main/java/com/home/mbm/work_hard/C.java
@@ -0,0 +1,52 @@
+package com.home.mbm.work_hard;
+
+import com.home.mbm.utils.ConnUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.QueueingConsumer;
+
+import java.io.IOException;
+
+/**
+* @Package: com.home.mbm.work_hard
+* @ClassName: C
+* @Description: RabbitMq 的消费者-1
+* @Author: mbm
+* @date: 2020/2/22 13:29
+* @Version: 1.0
+*/
+public class C {
+
+    private final static String QUEUE_NAME = "test_queue_work";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        // 获取连接
+        Connection conn = ConnUtils.getConn();
+        // 创建 通道
+        Channel channel = conn.createChannel();
+        // 声明队列
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+
+        // 同一时刻服务器只会发一条消息给消费者
+        channel.basicQos(1);
+
+        // 定义队列的消费者
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        // 监听队列
+        channel.basicConsume(QUEUE_NAME,false,consumer);
+
+        // 获取消息
+        while(true){
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String message = new String(delivery.getBody());
+            System.out.println(" [x] Received '" + message + "'");
+            // 休眠 10 毫秒
+            Thread.sleep(10);
+            // 返回确认状态
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(),false);
+        }
+
+    }
+
+}
